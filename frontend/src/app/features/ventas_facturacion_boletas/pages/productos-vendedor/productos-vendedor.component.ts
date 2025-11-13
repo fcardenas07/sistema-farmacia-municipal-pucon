f0dd9f5b84892224bb576313110d89a20892d7c1
Francisco Cárdenas <f.cardenas07@ufromail.cl>
2025-11-13 18:13:55 -0300

    feat(front): agregar navbar en productos-vendedor

--- frontend/src/app/features/ventas_facturacion_boletas/pages/productos-vendedor/productos-vendedor.component.ts
+++ frontend/src/app/features/ventas_facturacion_boletas/pages/productos-vendedor/productos-vendedor.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProductosTablaComponent } from '../../components/productos-tabla-vendedor/productos-tabla-vendedor.component';
+import { NavbarVendedorComponent } from "../../../../core/navbar-vendedor/navbar-vendedor.component";
 
 @Component({
   selector: 'app-productos-vendedor',
   standalone: true,
-  imports: [ProductosTablaComponent],
+  imports: [ProductosTablaComponent, NavbarVendedorComponent],
   templateUrl: './productos-vendedor.component.html',
   styleUrls: ['./productos-vendedor.component.css']
 })
