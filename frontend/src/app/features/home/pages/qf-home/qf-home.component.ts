fad48434c217d1b5b181f3df3efebee3238df7d8
Francisco Cárdenas <f.cardenas07@ufromail.cl>
2025-12-04 15:00:16 -0300

    feat: cambiar vista home-qf para ver lista de productos + opciones en navbar-qf

--- frontend/src/app/features/home/pages/qf-home/qf-home.component.ts
+++ frontend/src/app/features/home/pages/qf-home/qf-home.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import {NavbarQfComponent} from '../../../../core/navbar-qf/navbar-qf.component';
-import {OpcionesQfHomeComponent} from '../../components/opciones-qf-home/opciones-qf-home.component';
+import { NavbarQfComponent } from '../../../../core/navbar-qf/navbar-qf.component';
+import { ListaProductosQfComponent } from '../../../crear_productos_QF/components/lista-productos-qf/lista-productos-qf.component';
 
 @Component({
   selector: 'app-qf-home',
-  imports: [NavbarQfComponent, OpcionesQfHomeComponent],
+  imports: [NavbarQfComponent, ListaProductosQfComponent],
   templateUrl: './qf-home.component.html',
   standalone: true,
-  styleUrl: './qf-home.component.css'
+  styleUrl: './qf-home.component.css',
 })
-export class QfHomeComponent {
-
-}
+export class QfHomeComponent {}
