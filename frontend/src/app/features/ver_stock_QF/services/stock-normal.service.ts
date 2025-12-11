490863e80368271ff9234b07d1a6022e36427602
saliante01 <s.aliante01@ufromail.cl>
2025-12-11 01:07:38 -0300

    update(front): se cambia la ruta para recibir stock critico y normal

--- frontend/src/app/features/ver_stock_QF/services/stock-normal.service.ts
+++ frontend/src/app/features/ver_stock_QF/services/stock-normal.service.ts
@@ -10,7 +10,7 @@ import { ProductoFiltradoQF } from '../models/producto-filtrado-qf';
 })
 export class StockNormalService {
 
-  private API_URL = 'http://localhost:8080/productos/buscar-stock';
+  private API_URL = 'http://localhost:8080/productos/buscar';
 
   constructor(private http: HttpClient) {}
 
